e2764b2a886ae8dc3acba7788c2a51acc68230d4
adi247u <adi247u@gmail.com>
2022-12-15 22:48:05 +0530

    #1

---
+++ course-api/src/main/java/com/aditya/course/api/JavaBrainsCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.course.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsCourseApiApplication.class, args);
+	}
+
+}
---
+++ course-api/src/test/java/com/aditya/course/api/JavaBrainsCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.course.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBrainsCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
